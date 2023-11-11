d696204bd8e1df7d39eb17fe484907033c1955f7
MaksHalatyr <maksgalatir@gmail.com>
2023-11-11 06:20:39 +0200

    Готово

--- Task1.c
+++ Task1.c
@@ -1,65 +1,39 @@
 #include <stdio.h>
-#include <string.h>
 
-// Оголошення структури ZNAK
-struct ZNAK {
-    char name[50];
-    char zodiacSign[20];
-    int birthDate[3]; // Масив для дати народження
-};
+// Рекурсивна функція для обчислення X в ступені N
+double PowerN(double X, int N) {
+    if (N == 0) {
+        return 1;
+    } else if (N > 0 && N % 2 == 0) {
+        // Для парних N
+        double temp = PowerN(X, N / 2);
+        return temp * temp;
+    } else if (N > 0 && N % 2 != 0) {
+        // Для непарних N
+        return X * PowerN(X, N - 1);
+    } else if (N < 0) {
+        // Для від'ємних N
+        return 1 / PowerN(X, -N);
+    } else {
+        // Інші випадки
+        return 0; // Ви можете повертати інше значення або обробляти його відповідно до вашого варіанту
+    }
+}
 
 int main() {
-    struct ZNAK people[4];
-    int monthToSearch;
+    double X;
+    int N;
 
-    // Введення даних про людей
-    for (int i = 0; i < 4; i++) {
-        printf("Введіть дані для особи #%d:\n", i + 1);
+    // Введення значення X та N з клавіатури
+    printf("Введіть дійсне число X: ");
+    scanf("%lf", &X);
 
-        printf("Прізвище, ім'я: ");
-        scanf("%s", people[i].name);
+    printf("Введіть ціле число N: ");
+    scanf("%d", &N);
 
-        printf("Знак Зодіаку: ");
-        scanf("%s", people[i].zodiacSign);
-
-        printf("Дата народження (день місяць рік): ");
-        scanf("%d %d %d", &people[i].birthDate[0], &people[i].birthDate[1], &people[i].birthDate[2]);
-
-        printf("\n");
-    }
-
-    // Сортування за знаками Зодіаку
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3 - i; j++) {
-            if (strcmp(people[j].zodiacSign, people[j + 1].zodiacSign) > 0) {
-                // Обмін елементів місцями
-                struct ZNAK temp = people[j];
-                people[j] = people[j + 1];
-                people[j + 1] = temp;
-            }
-        }
-    }
-
-    // Введення місяця для пошуку
-    printf("Введіть номер місяця для пошуку (1-12): ");
-    scanf("%d", &monthToSearch);
-
-    // Виведення інформації про людей, що народилися в введеному місяці
-    int found = 0;
-    for (int i = 0; i < 4; i++) {
-        if (people[i].birthDate[1] == monthToSearch) {
-            printf("Прізвище, ім'я: %s\n", people[i].name);
-            printf("Знак Зодіаку: %s\n", people[i].zodiacSign);
-            printf("Дата народження: %02d.%02d.%04d\n", people[i].birthDate[0], people[i].birthDate[1], people[i].birthDate[2]);
-            printf("\n");
-            found = 1;
-        }
-    }
-
-    // Повідомлення, якщо таких людей немає
-    if (!found) {
-        printf("Немає людей, що народилися в введеному місяці.\n");
-    }
+    // Виклик рекурсивної функції та виведення результату
+    double result = PowerN(X, N);
+    printf("%lf в ступені %d дорівнює %lf\n", X, N, result);
 
     return 0;
 }
