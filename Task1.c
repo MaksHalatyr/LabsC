7805114469202ea7bec803b7ff2090200e338250
MaksHalatyr <maksgalatir@gmail.com>
2023-11-11 05:16:10 +0200

    Готово

--- Task1.c
+++ Task1.c
@@ -1,33 +1,64 @@
 #include <stdio.h>
-#include <ctype.h>
+#include <string.h>
+
+// Оголошення структури ZNAK
+struct ZNAK {
+    char name[50];
+    char zodiacSign[20];
+    int birthDate[3]; // Масив для дати народження
+};
 
 int main() {
-    char text[1000];
-    printf("Введіть текст: ");
-    fgets(text, sizeof(text), stdin);
+    struct ZNAK people[4];
+    int monthToSearch;
 
-    int countA = 0, countO = 0;
+    // Введення даних про людей
+    for (int i = 0; i < 4; i++) {
+        printf("Введіть дані для особи #%d:\n", i + 1);
 
-    for (int i = 0; text[i] != '\0'; ++i) {
-        char currentChar = tolower(text[i]); // перетворення на нижній регістр для порівняння
+        printf("Прізвище, ім'я: ");
+        scanf("%s", people[i].name);
 
-        if (currentChar == 'а') {
-            countA++;
-        } else if (currentChar == 'о') {
-            countO++;
+        printf("Знак Зодіаку: ");
+        scanf("%s", people[i].zodiacSign);
+
+        printf("Дата народження (день місяць рік): ");
+        scanf("%d %d %d", &people[i].birthDate[0], &people[i].birthDate[1], &people[i].birthDate[2]);
+
+        printf("\n");
+    }
+
+    // Сортування за знаками Зодіаку
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3 - i; j++) {
+            if (strcmp(people[j].zodiacSign, people[j + 1].zodiacSign) > 0) {
+                // Обмін елементів місцями
+                struct ZNAK temp = people[j];
+                people[j] = people[j + 1];
+                people[j + 1] = temp;
+            }
         }
     }
 
-    if (countA > 0 && countO > 0) {
-        if (countA > countO) {
-            printf("Символ 'а' зустрічається частіше (%d разів)\n", countA);
-        } else if (countO > countA) {
-            printf("Символ 'о' зустрічається частіше (%d разів)\n", countO);
-        } else {
-            printf("Символи 'а' та 'о' зустрічаються однакову кількість разів (%d разів)\n", countA);
+    // Введення місяця для пошуку
+    printf("Введіть номер місяця для пошуку (1-12): ");
+    scanf("%d", &monthToSearch);
+
+    // Виведення інформації про людей, що народилися в введеному місяці
+    int found = 0;
+    for (int i = 0; i < 4; i++) {
+        if (people[i].birthDate[1] == monthToSearch) {
+            printf("Прізвище, ім'я: %s\n", people[i].name);
+            printf("Знак Зодіаку: %s\n", people[i].zodiacSign);
+            printf("Дата народження: %02d.%02d.%04d\n", people[i].birthDate[0], people[i].birthDate[1], people[i].birthDate[2]);
+            printf("\n");
+            found = 1;
         }
-    } else {
-        printf("Один із символів ('а' або 'о') відсутній у тексті.\n");
+    }
+
+    // Повідомлення, якщо таких людей немає
+    if (!found) {
+        printf("Немає людей, що народилися в введеному місяці.\n");
     }
 
     return 0;
